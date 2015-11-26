2d4ff52453763c27ff4bfae3339e8c6fbef7bbb6
antondebruyn <debruynanton5@gmail.com>
2015-11-26 15:25:42 +0200

    Only managers and admins can delete tickets

--- spec/features/hidden_links_spec.rb
+++ spec/features/hidden_links_spec.rb
@@ -38,6 +38,11 @@ RSpec.feature "hidden links" do
       visit project_ticket_path(project, ticket)
       expect(page).not_to have_link "Edit Ticket"
     end
+
+    scenario "cannot see the Delete Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).not_to have_link "Delete Ticket"
+    end
   end
 
   context "admin users" do
@@ -65,6 +70,11 @@ RSpec.feature "hidden links" do
     scenario "can see the Edit Ticket link" do
       visit project_ticket_path(project, ticket)
       expect(page).to have_link "Edit Ticket"
+    end
+
+    scenario "can see the Delete Ticket link" do
+      visit project_ticket_path(project, ticket)
+      expect(page).to have_link "Delete Ticket"
     end
   end
 end
